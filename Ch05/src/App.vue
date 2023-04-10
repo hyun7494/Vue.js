d321330cc7dc892b98dd75e134a3c85a93dc380f
et009153 <et009153@gmail.com>
2023-04-10 09:53:58 +0900

    2023/04/10

---
+++ Ch05/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h3>Ch05.Router 실습</h3>
+  <h4>1) Router 기본</h4>
+  <router-link to="/hello">Hello</router-link> /
+  <router-link to="/welcome">Welcome</router-link> /
+  <router-link to="/greeting">Greeting</router-link>
+
+  <hr />
+  <h4>2) Nested Router</h4>
+  <router-link to="/parent">Parent</router-link>
+  <hr />
+  <h4>3) Named View</h4>
+  <router-link to="/main1">Main1</router-link>
+  <router-link to="/main2">Main2</router-link>
+  <router-link to="/main3">Main3</router-link>
+  <hr />
+
+  <router-view name="header"></router-view>
+  <router-view></router-view>
+  <router-view name="footer"></router-view>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ Ch05/src/routers/index.js
@@ -0,0 +1,64 @@
+/**
+ * Vue-Router 모듈파일 정의
+ */
+import { createRouter, createWebHistory } from "vue-router";
+import Hello from "../components/sub1/Hello.vue";
+import Welcome from "../components/sub1/Welcome.vue";
+import Greeting from "../components/sub1/Greeting.vue";
+import Parent from "../components/sub2/Parent.vue";
+import Child1 from "../components/sub2/Child1.vue";
+import Child2 from "../components/sub2/Child2.vue";
+import Header from "../components/sub3/Header.vue";
+import Footer from "../components/sub3/Footer.vue";
+import Main1 from "../components/sub3/Main1.vue";
+import Main2 from "../components/sub3/Main2.vue";
+import Main3 from "../components/sub3/Main3.vue";
+
+// router 인스턴스 생성
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/hello", name: "Hello", component: Hello },
+    { path: "/welcome", name: "Welcome", component: Welcome },
+    { path: "/greeting", name: "Greeting", component: Greeting },
+    {
+      path: "/parent",
+      name: "Parent",
+      component: Parent,
+      children: [
+        { path: "child1", name: "Child1", component: Child1 },
+        { path: "child2", name: "Child2", component: Child2 },
+      ],
+    },
+    {
+      path: "/main1",
+      name: "Main1",
+      components: {
+        default: Main1,
+        header: Header,
+        footer: Footer,
+      },
+    },
+    {
+      path: "/main2",
+      name: "Main2",
+      components: {
+        default: Main2,
+        header: Header,
+        footer: Footer,
+      },
+    },
+    {
+      path: "/main3",
+      name: "Main3",
+      components: {
+        default: Main3,
+        header: Header,
+        footer: Footer,
+      },
+    },
+  ],
+});
+
+// router 인스턴스 내보내기 -> main.js 에서 등록 설정
+export default router;
